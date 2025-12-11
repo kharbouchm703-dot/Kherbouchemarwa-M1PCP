bb0b480210a875c4ce0b66787f140499737a457d
kharbouchm703-dot <kharbouchm703@gmail.com>
2025-12-11 21:11:47 +0100

    Update marwa.py

--- marwa.py
+++ marwa.py
@@ -9,14 +9,8 @@ import pands as pd
 # Données : Séquences ADN , Longueur , Pourcentage de GC 
 data = {
     "Séquence": [
-        "ATGCGTACGTA",
-        "GCTAGCTAGGCC",
-        "ATGCGCGTAAGT",
-        "TACGATCGTA",
-        "ATGAAAGGCTT",
-        "CGTACCTAGC",
-        "TTAACCGGAT"
-    ],
+        "ATGCGTACGTA","GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACCTAGC",
+        "TTAACCGGAT"   ],
     "Longueur": [12, 12, 12, 10, 11, 10, 10],
     "Pourcentage_GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
 }
